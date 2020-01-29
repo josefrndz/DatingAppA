b8ebe1b6597dddcb2c6b98b49713b5d13e1171cf
Jose Fernandez <josefernandez@Joses-MacBook-Pro.local>
2020-01-29 10:53:56 -0500

    Initial Commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    // This route is translated to - http:localhost:5000/api/values
+    // Notice: [controller] is just a placeholder for 'values',
+    // so it's replaced as such in the url. .Net gets 'values' from
+    // our class name 'ValuesController' and knows to remove the 'Controller' part. 
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext context;
+        public ValuesController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        // GET api/values
+        // Returning 'IActionResult' allows us to return HTTP responses. 
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await this.context.Values.ToListAsync(); //context allows us access to EF commands
+
+            return Ok(values); //HTTP 200 OK response
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task <ActionResult> GetValue(int id)
+        {
+            //x represents the value we're returning
+            var value = await this.context.Values.FirstOrDefaultAsync(x => x.Id == id);
+
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        //Table name that gets created on our database
+        public DbSet<Value> Values {get; set;}
+    }
+}
